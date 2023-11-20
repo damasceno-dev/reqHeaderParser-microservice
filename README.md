010fd1115d4f9c02a94f3d7018260f4963fe7a3f
Fast Ninja <damasceno.developer21@gmail.com>
2023-11-19 22:53:28 -0300

    finished project with all tests passing

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Personal notes
 added typescript and tsx to boilerplate. dev script in package.json is running with watch flag to auto-detect code changes.
+link for replit.com: https://replit.com/@damascenodevelo/boilerplate-project-headerparser
+use the following url for tests checking: https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/request-header-parser-microservice
 
-# Timestamp Microservice
+# Request Header Parser Microservice
 
-This is the boilerplate code for the Timestamp Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+This is the boilerplate for the Request Header Parser Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/request-header-parser-microservice
--- index.ts
+++ index.ts
@@ -1,10 +1,6 @@
 import cors from 'cors'
 import express from 'express'
 
-// overflow y hidden 
-// 2) change 'link' styles  and colors to be like tailwind
-
-
 const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -19,50 +15,21 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint... 
-app.get("/api/hello", (req, res) => {
-  res.json({greeting: 'hello API'});
-});
+app.set('trust proxy', true)
 
-const isDateValid = (dateStr: string | undefined ) : Date | undefined => {
-  let dateObj: Date | undefined
-  if (!dateStr)   {
-    dateObj = new Date();
-  } else if(!isNaN(Number(dateStr))) {
-    dateObj = new Date();
-    dateObj.setTime(Number(dateStr))
-  }
-  else {
-    dateObj = new Date(dateStr);
-  }
-  console.log(dateObj)
-   
-  if (isNaN(dateObj.getTime())) {
-    return undefined
-  } else {
-    return dateObj
-  }
-}
 
-app.get('/api/:date?', (req, res) => {
-  const dateStr:string | undefined = req.params.date;
-  const date = isDateValid(dateStr)
-  if (date === undefined) {
-    return res.json({ error : "Invalid Date" })
-  } 
-
-  const utcDate = date.toUTCString();
-  const unixDate = date.getTime();
-  return res.json({unix: unixDate, utc: utcDate})
+app.get("/api/whoami", (req, res) => {
+  console.log(req.headers)
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  const ipaddress = req.ip;
+  console.log(req.ip)
+  return res.json({
+    "ipaddress": ipaddress,
+    "language": language,
+    "software": software
+  })
 })
-
-// app.get('/api/', (req, res)=> {
-//   console.log('opa')
-//   const date = new Date()
-//   const utcDate = date.toUTCString();
-//   const unixDate = Math.floor( date.getTime() /1000)
-//   return res.json({unix: unixDate, utc: utcDate})
-// })
 
 // listen for requests :)
 const port = process.env.PORT || 3333;
